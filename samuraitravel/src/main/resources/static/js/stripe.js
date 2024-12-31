170cc7d803a087fefd40c862fd00b14bd7458ee2
mmcarbonara <mmsloth1997@gmail.com>
2024-12-31 09:14:33 +0900

    コミット

---
+++ samuraitravel/src/main/resources/static/js/stripe.js
@@ -0,0 +1,8 @@
+ const stripe = Stripe('pk_test_51QbjX92LqmhYbMcTe3NaHV1ax0YgFCHsAiN8PXWGD3W4tFCKGCd1ThYOcDNx6aj82yittzilAGAUNSmdqITE4wtW00rOj1otfp');
+ const paymentButton = document.querySelector('#paymentButton');
+ 
+ paymentButton.addEventListener('click', () => {
+   stripe.redirectToCheckout({
+     sessionId: sessionId
+   })
+ });
