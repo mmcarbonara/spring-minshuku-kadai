f2f86e5bac147c638211e231bde510e90648ebde
mmcarbonara <mmsloth1997@gmail.com>
2025-01-13 15:51:58 +0900

    add

--- samuraitravel/src/main/resources/static/js/stripe.js
+++ samuraitravel/src/main/resources/static/js/stripe.js
@@ -1,4 +1,4 @@
-const stripe = stripe('pk_test_51QbjX92LqmhYbMcTe3NaHV1ax0YgFCHsAiN8PXWGD3W4tFCKGCd1ThYOcDNx6aj82yittzilAGAUNSmdqITE4wtW00rOj1otfp');
+const stripe = stripe(pk_test_51QbjX92LqmhYbMcTe3NaHV1ax0YgFCHsAiN8PXWGD3W4tFCKGCd1ThYOcDNx6aj82yittzilAGAUNSmdqITE4wtW00rOj1otfp);
 const paymentButton = document.querySelector('#paymentButton');
 
 paymentButton.addEventListener('click', () => {
